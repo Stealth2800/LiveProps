e8d66b19255a3ba7aefe84fd0f635c550599675e
Stealth2800 <stealth2800@stealthyone.com>
2014-02-13 19:17:39 -0800

    Add base plugin class

--- src/main/java/com/codeslingersmc/liveprops/LiveProps.java
+++ src/main/java/com/codeslingersmc/liveprops/LiveProps.java
@@ -4,4 +4,27 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class LiveProps extends JavaPlugin {
 
+    private static LiveProps instance;
+
+    public static LiveProps getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onLoad() {
+        instance = this;
+        getDataFolder().mkdir();
+    }
+
+    @Override
+    public void onEnable() {
+        getLogger().info("LiveProps v" + getDescription().getVersion() + " by " + getDescription().getAuthors().toString().replace("[", "").replace("]", "") + " ENABLED.");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("LiveProps v" + getDescription().getVersion() + " by " + getDescription().getAuthors().toString().replace("[", "").replace("]", "") + " DISABLED.");
+        instance = null;
+    }
+
 }
